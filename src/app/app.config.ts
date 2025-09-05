778498ec0c575483e2405a0b774fcd1e2df9d6c9
Miguel Mateo <miguelmateot94@gmail.com>
2025-09-05 10:10:25 -0400

    feat(settings): configure primeng and httpclient

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,66 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import Aura from '@primeuix/themes/aura';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { FilterMatchMode } from 'primeng/api';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(withFetch()),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      translation: {
+        /**
+         * accept: 'Aceptar',
+         * reject: 'Rechazar',
+         */
+      },
+      filterMatchModeOptions: {
+        text: [
+          FilterMatchMode.STARTS_WITH,
+          FilterMatchMode.CONTAINS,
+          FilterMatchMode.NOT_CONTAINS,
+          FilterMatchMode.ENDS_WITH,
+          FilterMatchMode.EQUALS,
+          FilterMatchMode.NOT_EQUALS,
+        ],
+        numeric: [
+          FilterMatchMode.EQUALS,
+          FilterMatchMode.NOT_EQUALS,
+          FilterMatchMode.LESS_THAN,
+          FilterMatchMode.LESS_THAN_OR_EQUAL_TO,
+          FilterMatchMode.GREATER_THAN,
+          FilterMatchMode.GREATER_THAN_OR_EQUAL_TO,
+        ],
+        date: [
+          FilterMatchMode.DATE_IS,
+          FilterMatchMode.DATE_IS_NOT,
+          FilterMatchMode.DATE_BEFORE,
+          FilterMatchMode.DATE_AFTER,
+        ],
+      },
+      zIndex: {
+        modal: 1100,
+        overlay: 1000,
+        menu: 1000,
+        tooltip: 1100,
+      },
+      ripple: true,
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: 'system',
+          cssLayer: false,
+        },
+      },
+    }),
+  ],
 };
